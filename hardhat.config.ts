2fc68ded09a135fa4b4edcecfc7c76475c42a1f0
ming2 <ming2@Mingders-Mac-mini.local>
2025-06-21 11:29:07 +0800

    update readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require("@nomicfoundation/hardhat-chai-matchers");
+
 require("dotenv").config();
 
 const config: HardhatUserConfig = {
